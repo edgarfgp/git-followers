4da55b362bd2659193a8d38d6ccb7abd0fdabfd0
Edgar <edgar.gonzalezpena@aviva.com>
2020-01-27 14:28:50 +0100

    Add some project assets and create new ViewControllers

---
+++ GitHubFollowers/FavouriteListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteListViewController.swift
+//  GitHubFollowers
+//
+//  Created by Edgar Gonzalez Pena on 27/01/2020.
+//  Copyright © 2020 Edgar Gonzalez Pena. All rights reserved.
+//
+
+import UIKit
+
+class FavouriteListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
--- GitHubFollowers/SceneDelegate.swift
+++ GitHubFollowers/SceneDelegate.swift
@@ -12,18 +12,40 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
+
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
+        window?.rootViewController = creteTabBar()
         window?.makeKeyAndVisible()
+    }
+    
+    func createSearchViewController () -> UINavigationController {
+        let searchVC = SearchViewController()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search , tag: 0)
         
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavouriteViewController () -> UINavigationController {
+        let favouriteVC = FavouriteListViewController()
+        favouriteVC.title = "Favourites"
+        favouriteVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites , tag: 1)
+        
+        return UINavigationController(rootViewController: favouriteVC)
+    }
+    
+    func creteTabBar() -> UITabBarController {
+        let tabBar = UITabBarController()
+        UITabBar.appearance().tintColor = .systemGreen
+        tabBar.viewControllers = [createSearchViewController() , createFavouriteViewController()]
+        
+        return tabBar
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
