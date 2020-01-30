da081795c81007e8edc4be5fc757bb92ee37ca7f
Edgar <edgar.gonzalezpena@aviva.com>
2020-01-30 14:50:10 +0100

    Add collectionViewCell and add some CustomImageView

---
+++ GitHubFollowers/CustomViews/FGAvatarImageView.swift
@@ -0,0 +1,31 @@
+//
+//  FGAvatarImageView.swift
+//  GitHubFollowers
+//
+//  Created by Edgar Gonzalez Pena on 30/01/2020.
+//  Copyright © 2020 Edgar Gonzalez Pena. All rights reserved.
+//
+
+import UIKit
+
+class FGAvatarImageView: UIImageView {
+    
+    private let placeHolderImage = UIImage(named: "avatar-placeholder")!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private  func configure(){
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeHolderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
---
+++ GitHubFollowers/CustomViews/FollowerCell.swift
@@ -0,0 +1,52 @@
+//
+//  FollowerCell.swift
+//  GitHubFollowers
+//
+//  Created by Edgar Gonzalez Pena on 30/01/2020.
+//  Copyright © 2020 Edgar Gonzalez Pena. All rights reserved.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    static let reuseID = "FollowerCell"
+    let padding: CGFloat = 8
+    
+    let avatarImageView = FGAvatarImageView(frame: .zero)
+    let userNameLabel = FGTitleLabel(textAligment: .center, fontSize: 16)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(follower: Follower){
+        userNameLabel.text = follower.login
+    }
+}
+
+extension FollowerCell {
+    private func configure(){
+        addSubview(avatarImageView)
+        addSubview(userNameLabel)
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            userNameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            userNameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            userNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            userNameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        
+    }
+}
