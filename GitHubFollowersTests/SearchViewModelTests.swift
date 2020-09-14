b26e9e088a745cf7e831e7070a2adb6fd5675a4c
Edgar Gonzalez <edgargonzalez.info@gmail.com>
2020-09-14 20:19:42 +0100

    Add SearchViewModel unit tests

--- GitHubFollowersTests/SearchViewModelTests.swift
+++ GitHubFollowersTests/SearchViewModelTests.swift
@@ -7,15 +7,34 @@
 //
 
 import XCTest
+import Combine
 @testable import GitHubFollowers
 
 class SearchViewModelTests: XCTestCase {
     
-    func testUserNameIsInitializeToEmty(){
+    private var subscriptions = Set<AnyCancellable>()
+    
+    override func tearDown() {
+        subscriptions = []
+    }
+    
+    func test_WithEmptyUserName_AlwaysReturnsIsValidUserNameFalse(){
+        let viewModel = SearchViewModel()
+        
+        viewModel.userName = ""
+        
+        viewModel.isValidUserName.sink(receiveValue: { value  in
+            XCTAssertFalse(value)
+        }).store(in: &subscriptions)
+    }
+    
+    func test_WithValidUserName_AlwaysReturnsIsValidUserNameTrue(){
         let viewModel = SearchViewModel()
         
         viewModel.userName = "edgarfgp"
         
-        XCTAssertFalse(viewModel.userName.isEmpty)
+        viewModel.isValidUserName.sink(receiveValue: { value  in
+            XCTAssertTrue(value)
+        }).store(in: &subscriptions)
     }
 }
